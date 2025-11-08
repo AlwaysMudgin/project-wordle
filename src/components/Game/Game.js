9d20beab3342cdf025497b1cc714c2d36d2c2698
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2025-11-07 22:20:55 -0500

    Stretch goals: - letterStatus state now tracks info needed for keyboard colors - handleSubmitGuess now also updates letterStatus as needed

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import GuessForm from "../GuessForm";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+import { checkGuess } from "../../game-helpers";
 import GuessResults from "../GuessResults/GuessResults";
 import WonBanner from "../WonBanner/WonBanner";
 import LostBanner from "../LostBanner/LostBanner";
@@ -16,13 +17,47 @@ console.info({ answer });
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
   const [gameStatus, setGameStatus] = React.useState("running");
+  const [letterStatus, setLetterStatus] = React.useState({});
+
   function handleSubmitGuess(tentativeGuess) {
     const nextGuesses = [...guesses, tentativeGuess];
     setGuesses(nextGuesses);
+    updateKeyboardLetters(tentativeGuess);
     if (tentativeGuess === answer) {
       setGameStatus("won");
     } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
       setGameStatus("lost");
+    }
+  }
+
+  function updateKeyboardLetters(word) {
+    const wordResults = checkGuess(word, answer);
+    let updates;
+    if (wordResults) {
+      updates = wordResults.reduce((differences, result) => {
+        let currentStatus = letterStatus[result.letter];
+        if (!currentStatus) {
+          // first use of letter will always update keyboard
+          return [...differences, result];
+        } else if (
+          currentStatus === "misplaced" &&
+          result.status === "correct"
+        ) {
+          // subsequent uses only update keyboard if misplaced => correct
+          return [...differences, result];
+        } else {
+          return differences;
+        }
+      }, []);
+    }
+    if (updates) {
+      const nextLetterStatus = {
+        ...letterStatus,
+      };
+      updates.forEach((update) => {
+        nextLetterStatus[update.letter] = update.status;
+      });
+      setLetterStatus(nextLetterStatus);
     }
   }
 
---
+++ src/components/Keyboard/Keyboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Keyboard() {
+  return (
+    <div className="keyboard-wrapper">
+      <div className="top-row"></div>
+      <div className="middle-row"></div>
+      <div className="bottom-row"></div>
+    </div>
+  );
+}
+
+export default Keyboard;
